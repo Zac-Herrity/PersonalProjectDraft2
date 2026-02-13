3aa733f78c4636dd5670bdc6e2a54115314ee7ed
Zac Herrity <S00269936@atu.ie>
2026-02-13 13:44:03 +0000

    created movie class will possibly change when API is introduced

---
+++ WpfApp1/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Movie
+    {
+        //Movie class with properties for Title, Genre, Director and Release Year
+
+        //Note - A lot of this is subject to change once I start working with an API to get movie data, but for now this is what I have in mind for the Movie class
+
+        //Properties
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseYear { get; set; }
+
+        //Constructor
+        public Movie(string title, string genre, string director, DateTime releaseYear)
+        {
+            Title = title;
+            Genre = genre;
+            Director = director;
+            ReleaseYear = releaseYear;
+        }
+        public Movie() { } //Empty constructor for when I start working with an API to get movie data
+
+    }
+}
