0f3b91093987b3aa036065a0cdfae7b505ea7178
Zac Herrity <S00269936@atu.ie>
2026-02-27 19:36:55 +0000

    created a user class Would like to have a create account / login feature as well as link seen movies / ratings tied to each user

---
+++ WpfApp1/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class User
+    {
+        //User Class, will be used for Login and Register features, will be stored in a Database and
+        //will also contain seen movies and rated movies per user
+
+        //Properties
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public List<string> SeenMovies { get; set; }
+        public int UserRating { get; set; }
+    }
+}
