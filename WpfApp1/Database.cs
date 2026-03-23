89e64580a79e6bfbbd00d3a7e2486d9e10c43688
ZacHerrity-STUDENT <ZacHerrity-STUDENT@S-4N1H504-SL26>
2026-03-23 10:39:05 +0000

    having issues with errors, pushing for safety

--- WpfApp1/Database.cs
+++ WpfApp1/Database.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Contexts;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Markup;
-using System.Windows.Media;
+using System.Data.SqlClient;
 
 namespace WpfApp1
 {
@@ -14,20 +8,24 @@ namespace WpfApp1
         //Class is to store db methods and connection string
 
         //connection string for local db (copied from db properties)
-        private string connectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = SceneItData; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False";
-
+        private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SceneItData;Integrated Security=True;";
         public bool UserValidation(string username, string password)
         {
             using (var connection = new System.Data.SqlClient.SqlConnection(connectionString))
-                {
+            { 
                 connection.Open();
                 var command = new System.Data.SqlClient.SqlCommand("SELECT COUNT(*) FROM Users WHERE Username = @username AND Password = @password", connection);
                 command.Parameters.AddWithValue("@username", username);
                 command.Parameters.AddWithValue("@password", password);
                 int result = (int)command.ExecuteScalar(); //scalar means it will return only one value
-                return result > 0;
+                return result > 0; //returns true if greater than 0
 
 
             }
+        }
+
+        public bool CreateUser(string username, string password)
+        {
+            using var connection = new System.Data.SqlClient.SqlConnection(connectionString);
+        }
     }
-}
