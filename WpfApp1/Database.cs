87b3ef46b361146d8dc48eb92bc10104e523221e
ZacHerrity-STUDENT <ZacHerrity-STUDENT@S-4N1H504-SL26>
2026-03-23 10:45:23 +0000

    created CreateUser and SaveRating methods

--- WpfApp1/Database.cs
+++ WpfApp1/Database.cs
@@ -12,7 +12,7 @@ namespace WpfApp1
         public bool UserValidation(string username, string password)
         {
             using (var connection = new System.Data.SqlClient.SqlConnection(connectionString))
-            { 
+            {
                 connection.Open();
                 var command = new System.Data.SqlClient.SqlCommand("SELECT COUNT(*) FROM Users WHERE Username = @username AND Password = @password", connection);
                 command.Parameters.AddWithValue("@username", username);
@@ -26,6 +26,28 @@ namespace WpfApp1
 
         public bool CreateUser(string username, string password)
         {
-            using var connection = new System.Data.SqlClient.SqlConnection(connectionString);
+            using (var connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new System.Data.SqlClient.SqlCommand("INSERT INTO Users (Username, Password) VALUES (@username, @password)", connection);
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", password);
+                int result = command.ExecuteNonQuery(); //nonquery means it will return the number of rows affected
+                return result > 0; //returns true if greater than 0
+            }
+        }
+
+        public void SaveRating(string username, int rating, string movieID)
+        {
+            using (var connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new System.Data.SqlClient.SqlCommand("INSERT INTO Ratings (Username, MovieID, Rating) VALUES (@username, @movieID, @rating)", connection);
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@movieID", movieID);
+                command.Parameters.AddWithValue("@rating", rating);
+                command.ExecuteNonQuery(); //nonquery means it will return the number of rows affected
+            }
         }
     }
+}
